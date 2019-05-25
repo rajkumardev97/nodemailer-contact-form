7ea03282391bc968e31e2c4751cf05599140edb8
rajkumardev97 <developer.rk97@gmail.com>
2019-05-25 18:38:14 +0530

    designed and developed the nodemailer contact form

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
-# nodemailer-contact-form
+# Nodemailer Contact Form
+
+Build Node.js/Express app using Nodemailer to send emails
+
+- Please add your own SMTP info for it to work
+
+## Install Dependencies
+
+```bash
+npm install
+```
+
+## Run
+
+```bash
+npm start
+```
---
+++ app.js
@@ -0,0 +1,74 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+const exphbs = require("express-handlebars");
+const path = require("path");
+const nodemailer = require("nodemailer");
+
+const app = express();
+
+// View engine setup
+app.engine("handlebars", exphbs());
+app.set("view engine", "handlebars");
+
+// Static folder
+app.use("/public", express.static(path.join(__dirname, "public")));
+
+// Body Parser Middleware
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+
+app.get("/", (req, res) => {
+  res.render("contact");
+});
+
+app.post("/send", (req, res) => {
+  //console.log(req.body);
+  const output = `
+  <p>You have a new contact request</p>
+  <h3>Contact Details</h3>
+  <ul>  
+    <li>Name: ${req.body.name}</li>
+    <li>Company: ${req.body.company}</li>
+    <li>Email: ${req.body.email}</li>
+    <li>Phone: ${req.body.phone}</li>
+  </ul>
+  <h3>Message</h3>
+  <p>${req.body.message}</p>
+`;
+
+  // create reusable transporter object using the default SMTP transport
+  let transporter = nodemailer.createTransport({
+    host: "mail.domain.com",
+    port: 25,
+    secure: false, // true for 465, false for other ports
+    auth: {
+      user: "test@domain.com", // your domain email address like test@domain.com
+      pass: "test@123" //password
+    },
+    tls: {
+      rejectUnauthorized: false
+    }
+  });
+
+  // setup email data with unicode symbols
+  let mailOptions = {
+    from: "test@domain.com", // sender address
+    to: "dashingprince101@gmail.com", // list of receivers
+    subject: "Node Contact Request 2", // Subject line
+    text: "Hello world?", // plain text body
+    html: output // html body
+  };
+
+  // send mail with defined transport object
+  transporter.sendMail(mailOptions, (error, info) => {
+    if (error) {
+      return console.log(error);
+    }
+    console.log("Message sent: %s", info.messageId);
+    console.log("Preview URL: %s", nodemailer.getTestMessageUrl(info));
+
+    res.render("contact", { msg: "Email has been sent" });
+  });
+});
+
+app.listen(3000, () => console.log("Server started..."));
